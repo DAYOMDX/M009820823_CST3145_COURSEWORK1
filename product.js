c5651eeac3d101419e303fc9ab09240a495664fb
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-03-30 01:06:13 +0400

    creating the JS file for products

---
+++ product.js
@@ -0,0 +1,93 @@
+let lessons = [
+    {
+        "id": 1001,
+        "image": "image/1001.jpg",
+        "subject": "Artiste",
+        "price": 100.00,
+        "location": "Dubai",
+        "description": "This lesson is for students who find interest in playing Chess and board games!",
+        "availability": 5
+    },
+    {
+        "id": 1002,
+        "image": "image/Best F.jpg",
+        "subject": "Explorer",
+        "price": 500.00,
+        "location": "Sydney",
+        "description": "This is an extra lesson session for students who wish to improve their exploring skills",
+        "availability": 5
+       
+    },
+    {
+        "id": 1003,
+        "image": "image/DML.jpg",
+        "subject": "music Art",
+        "price": 900.00,
+        "location": "England",
+        "description": "This is a session for students who wish to improve their music skills and knowlegde",
+        "availability": 5
+    },
+    {
+        "id": 1004,
+        "image": "image/Free 2.jpg",
+        "subject": "English",
+        "price": 70.00,
+        "location": "Ghana",
+        "description": "Join us and improve your English Language skills",
+        "availability": 5
+    },
+    {
+        "id": 1005,
+        "image": "image/Poli.jpg",
+        "subject": "politics",
+        "price": 500.00,
+        "location": "Dubai",
+        "description": "This course is for students who wish to improve their political understanding!.",
+        "availability": 5
+    },
+    {
+        "id": 1006,
+        "image": "image/code.png",
+        "subject": "Computer-Science",
+        "price": 300.00,
+        "location": "London",
+        "description": "Come to expand your knowledge on the fundamentals of web and app Development",
+        "availability": 5
+    },
+    {
+        "id": 1007,
+        "image": "image/Best F.jpg",
+        "subject": "Fine-Art",
+        "price": 200.00,
+        "location": "Abu-Dhabi",
+        "description": "This session is for students with little to no drawing skills to learn basic of art",
+        "availability": 5
+    },
+    {
+        "id": 1008,
+        "image": "image/MINIING.jpg",
+        "subject": "Engineering",
+        "price": 30.00,
+        "location": "Glasgow",
+        "description": "This is a make up classes for the Engineering lectures",
+        "availability": 5
+    },
+    {
+        "id": 1009,
+        "image": "image/Roots.jpg",
+        "subject": "Culture 101",
+        "price": 800.00,
+        "location": "Nigeria",
+        "description": "Come for the basic tutoring on learing your culture ",
+        "availability": 5
+    },
+    {
+        "id": 1010,
+        "image": "image/code.png",
+        "subject": "Music",
+        "price": 400.00,
+        "location": "Houston",
+        "description": "This is for all Music Fanatics, come for tutoring in music.",
+        "availability": 5
+    }
+]
